18569323f1eb9a3a6d11434afca2cc4ffaf7b0ab
Rachel Henderson <rhenderson1993@gmail.com>
2021-06-08 09:16:55 -0400

    updated comment to reflect new enthusiastic SayHi function output

--- function_SayHi.R
+++ function_SayHi.R
@@ -2,7 +2,7 @@
 # making another comment so that we can test things.
 SayHi <- function(yourname){
   # A function that takes a name
-  # Returns "Hello Yourname"
+  # Returns "Hello yourname!"
     print(paste("Hello ", yourname, "!", sep = ""))
   }
 
