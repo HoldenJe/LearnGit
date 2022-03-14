ac51407c8a6e8489d207e222cda56a86d19c86c1
Rachel Henderson <rhenderson1993@gmail.com>
2022-03-14 10:00:34 -0400

    delete old files?

--- make_ggplot.R
+++
@@ -1,8 +0,0 @@
-# make a ggplot for the whole world to see
-library(ggplot2)
-df <- data.frame(x = 1:10, y= seq(10, 100, 10))
-
-ggplot(df, aes(x, y)) + geom_point() + 
-  geom_smooth(se=F) + theme_classic()
-
-#this is cool
